7657dbbc5d875ed93885f82538933eb705d752de
JackalsTensor <jackals833@qq.com>
2026-06-09 15:16:18 +0800

    Update 输入输出函数模板.cpp

--- C++/函数/输入输出函数模板.cpp
+++ C++/函数/输入输出函数模板.cpp
@@ -1,14 +1,14 @@
 void input(int a[],int n)
 {
     for(int i=0;i<n;i++)
-        {
-            cin>>a[i];
-        }
+    {
+        cin>>a[i];
+    }
 }
 void output(int a[],int n)
 {
     for(int i=0;i<n;i++)
-        {
-            cout<<a[i]<<" ";
-        }
+    {
+        cout<<a[i]<<" ";
+    }
 }
