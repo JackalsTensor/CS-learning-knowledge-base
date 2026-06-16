793ce1c6774861834dc8983342d8b4a8973db647
JackalsTensor <jackals833@qq.com>
2026-06-16 21:11:20 +0800

    Create LED闪烁.c

---
+++ 51单片机/LED/LED闪烁.c
@@ -0,0 +1,21 @@
+#include <REGX52.H>
+
+void Delay(unsigned int t)
+{
+    unsigned int i,j;
+    for(i=t;i>0;i--)
+        for(j=110;j>0;j--);
+
+}
+
+
+void main()
+{
+    while(1)
+    {
+        P2 = 0x00;   
+        Delay(500);
+        P2 = 0xFF;   
+        Delay(500);
+    }
+}
