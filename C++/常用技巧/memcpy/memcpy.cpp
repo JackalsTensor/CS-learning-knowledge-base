ff89b75872471272b05d788e0b9dc9b18f6b472e
JackalsTensor <jackals833@qq.com>
2026-08-14 02:38:58 +0800

    Create memcpy.cpp

---
+++ C++/常用技巧/memcpy/memcpy.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    int a[10] = {1,2,3,4,5,6,7,8,9,10};
+    int b[10] = {0};
+    memcpy(b, a, 10 * sizeof(int));
+    for(int e: b)
+    {
+        cout << e << " ";
+    }
+    return 0;
+}
