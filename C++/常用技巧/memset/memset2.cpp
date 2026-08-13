7306cd2749d1f73c7d04e02d5e1ed24d2e4146c8
JackalsTensor <jackals833@qq.com>
2026-08-13 23:01:46 +0800

    Update memset2.cpp

--- C++/常用技巧/memset/memset2.cpp
+++ C++/常用技巧/memset/memset2.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include<cstring>
+using namespace std;
 
+int main()
+{
+  int arr[5]={1,2,3,4,5};
+  memset(arr,0,20);
+  
+  for(int e:arr)
+  {
+    cout<<e<<" ";    
+  }
+  cout<<endl;
+  return 0;
+}
+
+/"
+0 0 0 0 0                                                                                                               
+                   
+"/
